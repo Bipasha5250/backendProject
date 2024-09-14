680925933049ea6cf0cb8dfef6c8dae0043b201b
Bipasha Bareja <bipashabareja1@gmail.com>
2024-09-14 17:31:12 +0530

    listing

---
+++ models/listing.js
@@ -0,0 +1,14 @@
+const mongoose =require("mongoose");
+const Schema=mongoose.Schema;
+ const listingSchema=new Schema({
+    title:{
+       type: String,
+       required:true,
+    },
+    description:String,
+    image:String,
+    price:Number,
+    location:String,
+    country:String,
+
+ })
